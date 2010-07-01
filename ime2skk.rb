5cd439a7fc9c7261b1879ff86b8c53d0ae4d6018
Kenta Murata <mrkn@mrkn.jp>
2010-07-01 20:28:38 +0900

    * new script for converting MS-IME's dictionary file into SKK-JISYO format.

---
+++ ime2skk.rb
@@ -0,0 +1,13 @@
+Encoding.default_external = Encoding.find("CP932")
+while line = gets
+  line.chomp!
+  line.strip!
+  next if line.empty?
+  next if line =~ /^\!/
+
+  a, b, c = line.split(/\t/)
+  begin
+    puts "#{a} /#{b}/".encode("EUC-JP-ms")
+  rescue
+  end
+end
